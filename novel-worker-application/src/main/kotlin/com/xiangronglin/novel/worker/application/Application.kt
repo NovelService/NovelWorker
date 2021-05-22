89981edb3cdace881c69ad8e622748a0e9dc3b4b
XiangRongLin <41164160+XiangRongLin@users.noreply.github.com>
2021-05-22 17:36:59 +0200

    Rename class NovelWorkerApplication to match filename

--- novel-worker-application/src/main/kotlin/com/xiangronglin/novel/worker/application/Application.kt
+++ novel-worker-application/src/main/kotlin/com/xiangronglin/novel/worker/application/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class NovelWorkerApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<NovelWorkerApplication>(*args)
+    runApplication<Application>(*args)
 }
